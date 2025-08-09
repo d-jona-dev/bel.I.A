d3f1d93ad8219c2c91aea319b84560c9bf221852
Maugan-Ra <d-jona@hotmail.be>
2025-08-09 21:34:10 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/avatars/page.tsx
+++ src/app/avatars/page.tsx
@@ -23,6 +23,8 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
+  DialogFooter,
 } from "@/components/ui/dialog";
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
